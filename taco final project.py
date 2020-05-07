5022a2ecdcc8ab5637288b8a7d0ec98d9ea80007
tj2586yk <59978398+tj2586yk@users.noreply.github.com>
2020-05-06 19:51:01 -0500

    start

--- taco final project.py
+++ taco final project.py
@@ -1,1 +1,7 @@
-import requests, PIL, docx
+import requests,PIL,docx
+taco_url = 'https://raw.github.com/sinker/tacofancy/master/seasonings/sriracha_salt.md'
+taco_recipe = requests.get(taco_url).json()
+
+print(taco_url)
+
+
