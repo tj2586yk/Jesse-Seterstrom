be2209e0c039db68b221e93abdb236add6c133f1
tj2586yk <59978398+tj2586yk@users.noreply.github.com>
2020-05-08 18:47:00 -0500

    start

--- taco final project.py
+++ taco final project.py
@@ -2,6 +2,12 @@ import requests,PIL,docx
 taco_url = 'https://raw.github.com/sinker/tacofancy/master/seasonings/sriracha_salt.md'
 taco_recipe = requests.get(taco_url).json()
 
-print(taco_url)
+document = docx.Document()
+document.add_paragraph('Taco Recipe!!,  Title')
+
+
+
+
+document.save('taco_url.docx')
 
 
