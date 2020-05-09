d048da1b97769d0d5a2cef4c979afbd359dbb132
tj2586yk <59978398+tj2586yk@users.noreply.github.com>
2020-05-09 14:05:16 -0500

    start

--- taco final project.py
+++ taco final project.py
@@ -2,6 +2,12 @@ import requests,PIL,docx
 taco_url = 'https://raw.github.com/sinker/tacofancy/master/seasonings/sriracha_salt.md'
 taco_recipe = requests.get(taco_url).json()
 
-print(taco_url)
+document = docx.Document()
+document.add_paragraph('Taco Recipe', 'Title')
 
 
+image = Image.open('photo-1562059390-a761a084768e.jpg')
+image.save('photo-1562059390-a761a084768e.jpg')
+
+
+document.save('Taco Recipe.docx')
