883901ac04a5a41b9f377ec604619d162e2dd2e2
tj2586yk <59978398+tj2586yk@users.noreply.github.com>
2020-05-10 16:43:34 -0500

    start

--- taco final project.py
+++ taco final project.py
@@ -1,13 +1,17 @@
-import requests,PIL,docx
-taco_url = 'https://raw.github.com/sinker/tacofancy/master/seasonings/sriracha_salt.md'
-taco_recipe = requests.get(taco_url).json()
+import requests, docx,PIL
+
+
+taco_url = 'https://api.taco-1150.herokuapp.com/random/?full_taco=true'
+response = requests.get(taco_url).json()
 
 document = docx.Document()
-document.add_paragraph('Taco Recipe', 'Title')
+document.add_heading('Taco Recipe', 'Title')
 
 
-image = Image.open('photo-1562059390-a761a084768e.jpg')
-image.save('photo-1562059390-a761a084768e.jpg')
+
+
+document.add_picture('photo-1562059390-a761a084768e.jpg')
+
 
 
 document.save('Taco Recipe.docx')
