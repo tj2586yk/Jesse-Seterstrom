13acbab9d76c840401065df5396c108bde3e332c
tj2586yk <59978398+tj2586yk@users.noreply.github.com>
2020-05-12 15:10:41 -0500

    start

--- taco final project.py
+++ taco final project.py
@@ -1,11 +1,30 @@
 import requests, docx,PIL
 
 
-taco_url = 'https://api.taco-1150.herokuapp.com/random/?full_taco=true'
+taco_url = 'https://taco-1150.herokuapp.com/random/?full_taco=true'
 response = requests.get(taco_url).json()
 
 document = docx.Document()
-document.add_heading('Taco Recipe', 'Title')
+document.add_heading('Taco Recipe', level=1)
+def taco_data():
+    response = requests.get(taco_url).json()
+    seasoning = response['seasoning']
+
+    condiment = response['condiment']
+    mixin = response['mixin']
+    base_layer = response['base_layer']
+    shell = response['shell']
+    print(seasoning)
+    print(condiment)
+    print(mixin)
+    print(base_layer)
+    print(shell)
+    return seasoning,condiment,mixin,base_layer,shell
+
+
+def taco_recipe_body():
+    seasoning, condiment, mixin, base_layer, shell = taco_data()
+    document.add_heading(f'{base_layer["name"]} with {mixin["name"]} with {shell["name"]} with {seasoning["name"]}')
 
 
 
