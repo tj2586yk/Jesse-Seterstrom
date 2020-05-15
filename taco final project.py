4f33c2e85cdfac1fc46984cd78af85d92fa0b6cc
tj2586yk <59978398+tj2586yk@users.noreply.github.com>
2020-05-15 16:58:33 -0500

    piture

--- taco final project.py
+++ taco final project.py
@@ -5,9 +5,9 @@ taco_recipe = requests.get(taco_url).json()
 document = docx.Document()
 document.add_paragraph('Taco Recipe!!,  Title')
 
-
-
-
+image = Image.open('photo-1582234372722-50d7ccc30ebd.jpg')
+img_draw = ImageDraw.Draw(image)
+taco final project.py:10
 document.save('taco_url.docx')
 
 
